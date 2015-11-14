4a1a61ae7926c1c3eebea26f324159dbb0a1c6eb
NPOLA <npola@usc.edu>
2015-11-14 15:28:22 -0800

    Integrated Akhil's Network code

--- WiseBiteSwift/WiseBiteSwift WatchKit Extension/Search.swift
+++ WiseBiteSwift/WiseBiteSwift WatchKit Extension/Search.swift
@@ -33,8 +33,32 @@ class Search: WKInterfaceController {
         super.didDeactivate()
     }
     
+    func httpGet(request: NSURLRequest!, callback: (String, String?) -> Void) {
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request){
+            (data, response, error) -> Void in
+            if error != nil {
+                callback("", error!.localizedDescription)
+            } else {
+                let result = NSString(data: data!, encoding:
+                    NSASCIIStringEncoding)!
+                callback(result as String, nil)
+            }
+        }
+        task.resume()
+    }
+    
     func getSearchResults(text: String) {
-        
+        //Try for cheese
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://calorie-checker.azurewebsites.net/search?keywords[0]=cheese")!)
+        httpGet(request){
+            (data, error) -> Void in
+            if error != nil {
+                print(error)
+            } else {
+                print(data)
+            }
+        }
     }
     
     func displayInfo(var results: String){
