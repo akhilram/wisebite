e03782eb93f2cd7bbdc815bcad488f60fcd6b63f
Akhil Ramachandran <akhilram@usc.edu>
2015-11-14 10:01:12 -0800

    food info api done

--- calorie-checker/app.js
+++ calorie-checker/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var search = require('./routes/search');
+var info = require('./routes/info');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/search', search);
+app.use('/info', info);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
