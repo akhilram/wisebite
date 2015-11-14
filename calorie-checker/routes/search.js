b2d074a417dd2c17bd9278098213a1715c9c90d5
Akhil Ramachandran <akhilram@usc.edu>
2015-11-14 02:35:38 -0800

    rest call finished

--- calorie-checker/routes/search.js
+++ calorie-checker/routes/search.js
@@ -1,9 +1,36 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
+var bodyParser = require('body-parser');
+var async = require('async');
+var request = require('request');
 
-/* GET users listing. */
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
+
+
+/* GET food listing. */
 router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+
+    var keywords = req.query.keywords;
+
+    // get search results each keywords in parallel
+    async.map(keywords, getFoodDetailsFromNDB,
+        function (error, results) {
+            res.send(results);
+        });
 });
+
+// callback to get search results
+function getFoodDetailsFromNDB(keyword, callback) {
+
+    var url = 'http://api.nal.usda.gov/ndb/search?format=json&lt=f&sort=r&api_key=cK9v2xEPobkXTTEQNmvGN3ndRjuc0n6t4w2Psfj4'
+        + '&q=' + keyword;
+    console.log(url);
+    request(url, function(error, response, body) {
+        body = JSON.parse(body);
+        callback(error, body.list.item);
+    });
+}
 
 module.exports = router;
